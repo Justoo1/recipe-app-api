04f64126f989223f969137691db37384de883134
Justoo1 <amankrahjay@gmail.com>
2020-09-21 13:22:48 +0530

    Added manageruser endpoint api

--- app/user/views.py
+++ app/user/views.py
@@ -11,7 +11,7 @@ class CreateUserView(generics.CreateAPIView):
 
 
 class CreateTokenView(ObtainAuthToken):
-    """Create a new auth token for user"""
+    """Create a new auth token for a user"""
     serializer_class = AuthTokenSerializer
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
 
